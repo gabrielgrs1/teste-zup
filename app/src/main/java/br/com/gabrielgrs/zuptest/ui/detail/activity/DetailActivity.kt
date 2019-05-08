c009d7981a911a09a5d9d33581717b5e3703be88
Gabriel Guilherme Rodrigues Silva <gabriel_guilherme2006@hotmail.com>
2019-05-08 01:28:40 -0300

    Impement detail

--- app/src/main/java/br/com/gabrielgrs/zuptest/ui/detail/activity/DetailActivity.kt
+++ app/src/main/java/br/com/gabrielgrs/zuptest/ui/detail/activity/DetailActivity.kt
@@ -1,14 +1,115 @@
 package br.com.gabrielgrs.zuptest.ui.detail.activity
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
 import br.com.gabrielgrs.zuptest.R
+import br.com.gabrielgrs.zuptest.model.movie.MovieDto
+import br.com.gabrielgrs.zuptest.ui.detail.viewmodel.DetailViewModel
+import br.com.gabrielgrs.zuptest.utils.IGenerickCallback
+import br.com.gabrielgrs.zuptest.utils.forceHideKeyboard
 import br.com.gabrielgrs.zuptest.utils.initActivity
+import br.com.gabrielgrs.zuptest.utils.makeToast
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_detail.*
 
-class DetailActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity(), IGenerickCallback {
+
+
+    private lateinit var viewModel: DetailViewModel
+    private lateinit var movie: MovieDto
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initActivity(R.layout.activity_detail)
+
+        init()
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_detail, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_delete -> {
+                performDeleteButton()
+                return true
+            }
+            R.id.action_save -> {
+                performSaveButton()
+                return true
+            }
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onError(error: String) {
+        forceHideKeyboard()
+        makeToast(error)
+    }
+
+    private fun performSaveButton() {
+        viewModel.saveMovie(movie)
+        makeToast(getString(R.string.detail_movie_saved_toast))
+    }
+
+    private fun performDeleteButton() {
+        viewModel.deleteMovie(movie)
+        makeToast(getString(R.string.detail_movie_deleted_toast))
+    }
+
+    private fun initActionBar() {
+        setSupportActionBar(detail_toolbar)
+        val actionBar = supportActionBar
+        actionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+
+    private fun init() {
+        initViewModel()
+        getMovieExtras()
+        initActionBar()
+    }
+
+    private fun getMovieExtras() {
+        if (intent != null && intent.extras != null) {
+            movie = intent.extras!!.getSerializable("movie") as MovieDto
+
+            setBanner()
+            setTexts()
+        } else {
+            makeToast(getString(R.string.generic_error_get_movie))
+            finish()
+        }
+
+
+    }
+
+    private fun setBanner() {
+        Glide.with(this)
+            .load(movie.poster)
+            .into(detail_banner_imageview)
+    }
+
+    private fun setTexts() {
+        val title = movie.title + " " + movie.year
+        detail_title_textview.text = title
+        detail_plot_textview.text = movie.plot
+    }
+
+    private fun initViewModel() {
+        viewModel = ViewModelProviders.of(this).get(DetailViewModel::class.java)
+        viewModel.setCallback(this)
+    }
+
 }
--- app/src/main/java/br/com/gabrielgrs/zuptest/ui/detail/viewmodel/DetailViewModel.kt
+++ app/src/main/java/br/com/gabrielgrs/zuptest/ui/detail/viewmodel/DetailViewModel.kt
@@ -1,10 +1,61 @@
 package br.com.gabrielgrs.zuptest.ui.detail.viewmodel
 
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import br.com.gabrielgrs.zuptest.application.ZupTestApplication
+import br.com.gabrielgrs.zuptest.model.movie.MovieDto
+import br.com.gabrielgrs.zuptest.utils.IGenerickCallback
+import com.orhanobut.hawk.Hawk
+
 /**
  * Created by gabrielgrs
  * Date: 29/04/19
  * Time: 21:24
  * Project: ZUPTest
  */
-class DetailViewModel {
+class DetailViewModel : ViewModel() {
+
+    private lateinit var callback: IGenerickCallback
+    private val TAG = "DetailViewModel"
+
+    fun setCallback(callback: IGenerickCallback) {
+        this.callback = callback
+    }
+
+    fun saveMovie(movie: MovieDto) {
+        val movieList: MutableList<MovieDto> = Hawk.get(ZupTestApplication.MOVIE_LIST_KEY, mutableListOf())
+
+        if (!movieList.contains(movie)) {
+            movieList.add(movie)
+            Hawk.put(ZupTestApplication.MOVIE_LIST_KEY, movieList)
+        } else {
+            Log.e(TAG, "Filme já está salvo!")
+
+        }
+    }
+
+    fun deleteMovie(movie: MovieDto) {
+        val movieList: MutableList<MovieDto> = Hawk.get(ZupTestApplication.MOVIE_LIST_KEY, mutableListOf())
+
+        if (movieList.contains(movie)) {
+            movieList.remove(movie)
+            Hawk.put(ZupTestApplication.MOVIE_LIST_KEY, movieList)
+        } else {
+            Log.e(TAG, "Filme nao está na lista!")
+
+        }
+    }
+
+
+    fun getMovies(): MutableList<MovieDto> {
+        var movieList: MutableList<MovieDto> = mutableListOf()
+
+        if (Hawk.get(ZupTestApplication.MOVIE_LIST_KEY, mutableListOf<MovieDto>()).isNotEmpty()) {
+            movieList = Hawk.get(ZupTestApplication.MOVIE_LIST_KEY)
+        } else {
+            Log.e(TAG, "Nenhum filme encontrado!")
+        }
+
+        return movieList
+    }
 }
